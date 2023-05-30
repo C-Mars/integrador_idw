f25c1b50131ce7ec7e550450fc3c48459ed15d6b
marilin <puchetamarilin@gmail.com>
2023-05-30 17:37:25 -0300

    Cambios
    nav, pág de convocatorias. se agregó la carpeta de Bootstrap

--- js/script.js
+++ js/script.js
@@ -1,7 +1,38 @@
 
 
 // *************** Consulta ********************* //
+// function enviar(){
+//     let nombreApellido = document.getElementById("nombreyapellido");
+//     alert(nombreApellido)
+// document.getElementById('leerLocalStorgeMejorado').addEventListener('click',leerLocalStorgeMejorado);
+document.getElementById('enviar').addEventListener('click',enviar);
+
 function enviar(){
-    let nombreApellido = document.getElementById("nombreyapellido");
-    alert(nombreApellido)
+    const jsonContacto = localStorage.getItem('contacto');
+
+    // si hay datos agrego al lo que haya en el local
+    if(jsonContacto !== null){
+        const jsonData = JSON.parse(jsonContacto);
+
+        let id = jsonData.length;
+
+        const contactoNuevo = {
+            'id': id,
+            'nombreyapellido': document.getElementById('nombreyapellido').value,
+            'email': document.getElementById('email').value,
+            'asunto': document.getElementById('asunto').value,
+            'comentario': document.getElementById('comentario').value,
+        }
+        
+        jsonData.push(contactoNuevo);
+        
+        // console.log(jsonData);
+
+        // elimino del local la clave alumnos
+        localStorage.removeItem('contacto');
+
+        const guardar = JSON.stringify(jsonData);
+        
+        localStorage.setItem('contacto', guardar);
+    }
 }
