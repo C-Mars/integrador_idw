ecda96f3df1110d74681205d7e43ffe21ee11515
marilin <puchetamarilin@gmail.com>
2023-06-01 22:31:12 -0300

    Error JS
    error en la dirección de js en las páginas de html internas.

--- js/script.js
+++ js/script.js
@@ -5,34 +5,42 @@
 //     let nombreApellido = document.getElementById("nombreyapellido");
 //     alert(nombreApellido)
 // document.getElementById('leerLocalStorgeMejorado').addEventListener('click',leerLocalStorgeMejorado);
-document.getElementById('enviar').addEventListener('click',enviar);
+// document.getElementById('enviar').addEventListener('click',enviar);
 
-function enviar(){
-    const jsonContacto = localStorage.getItem('contacto');
+// function enviar(){
+//     const jsonContacto = localStorage.getItem('contacto');
 
-    // si hay datos agrego al lo que haya en el local
-    if(jsonContacto !== null){
-        const jsonData = JSON.parse(jsonContacto);
+//     // si hay datos agrego al lo que haya en el local
+//     if(jsonContacto !== null){
+//         const jsonData = JSON.parse(jsonContacto);
 
-        let id = jsonData.length;
+//         let id = jsonData.length;
 
-        const contactoNuevo = {
-            'id': id,
-            'nombreyapellido': document.getElementById('nombreyapellido').value,
-            'email': document.getElementById('email').value,
-            'asunto': document.getElementById('asunto').value,
-            'comentario': document.getElementById('comentario').value,
-        }
+//         const contactoNuevo = {
+//             'id': id,
+//             'nombreyapellido': document.getElementById('nombreyapellido').value,
+//             'email': document.getElementById('email').value,
+//             'asunto': document.getElementById('asunto').value,
+//             'comentario': document.getElementById('comentario').value,
+//         }
         
-        jsonData.push(contactoNuevo);
+//         jsonData.push(contactoNuevo);
         
-        // console.log(jsonData);
+//         // console.log(jsonData);
 
-        // elimino del local la clave alumnos
-        localStorage.removeItem('contacto');
+//         // elimino del local la clave alumnos
+//         localStorage.removeItem('contacto');
 
-        const guardar = JSON.stringify(jsonData);
+//         const guardar = JSON.stringify(jsonData);
         
-        localStorage.setItem('contacto', guardar);
-    }
+//         localStorage.setItem('contacto', guardar);
+//     }
+// }
+
+/*convocatorias */
+
+document.getElementById('nueva-conv').addEventListener('click',crearConvocatoria );
+
+function crearConvocatoria(){
+    window.location.href= 'crearconvocatoria.html'
 }
